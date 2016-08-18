35454b68a23b5324c76ca9482a7c45bf1189d399
bestseob93 <bestseob93>
2016-08-18 20:46:56 +0900

    1234

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160804152932) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "soundcloud_user_id"
-    t.string   "soundcloud_access_token"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
