ad63713038e0703147365155a743fc057f505281
Hyunwoo <cho7673@gmail.com>
2016-08-24 02:51:37 +0900

    MARK 띄우기 완성!!

--- app/controllers/cmap_controller.rb
+++ app/controllers/cmap_controller.rb
@@ -27,9 +27,9 @@ class CmapController < ApplicationController
       json_doc = JSON.parse(res.body)
 
       # 파싱된 객체(json_doc)로부터 주소 추출
-      # address = json_doc['result']['items'].at(0)['address']
+      address = json_doc['result']['items'].at(0)['address']
 
-      return json_doc
+      return address
     end
 
     # 주소를 위/경도 좌표로 변환하는 함수 (Naver Geocode API)
@@ -64,13 +64,14 @@ class CmapController < ApplicationController
       return lat, lng
     end
 
+    # 이전 View에서 request한 위치정보를 변수에 저장
     @currentLat = params[:lat]
     @currentLng = params[:lng]
-    @currentAddress = latlngToAddress(@currentLat, @currentLng)
+    currentAddress = latlngToAddress(@currentLat, @currentLng)
 
     ##### 검색 API 사용 > 노래방 검색 #####
     #query = params[:query]
-    queryUrl = "https://openapi.naver.com/v1/search/local.xml?query="+"코인노래방"
+    queryUrl = "https://openapi.naver.com/v1/search/local.xml?query="+currentAddress+" 코인노래방"
 
     uri = URI(URI.encode(queryUrl))
 
